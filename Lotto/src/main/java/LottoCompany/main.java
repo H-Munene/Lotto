80b46619ec6c258665ad884c2598db43541f0607
Hezekiah M Munene <hezemunene01@gmail.com>
2023-04-26 04:46:38 +0300

    cleared uninitialized method

--- Lotto/src/main/java/LottoCompany/main.java
+++ Lotto/src/main/java/LottoCompany/main.java
@@ -2,12 +2,11 @@ package LottoCompany;
 
 public class main {
     public static void main(String[] args){
-       /* LottoPlayer instance1 = new LottoPlayer();
+        LottoPlayer instance1 = new LottoPlayer();
         instance1.user_details();
-        instance1.make_selection();*/
+        instance1.make_selection();
 
         LottoSys instance2 = new LottoSys();
-        instance2.create_gameid();
         instance2.generate_winning_numbers();
 //        instance2.compare_selection_winNumbs(instance1);
         }
