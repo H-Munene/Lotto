7371d7274f4393e25a1c8f3d67afeb6237a662e4
Hezekiah M Munene <hezemunene01@gmail.com>
2023-04-26 03:31:58 +0300

    updated the connection class

--- Lotto/src/main/java/LottoCompany/DbConnection.java
+++ Lotto/src/main/java/LottoCompany/DbConnection.java
@@ -1,6 +1,7 @@
 package LottoCompany;
 
 
+import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -10,7 +11,25 @@ import org.bson.Document;
 public class DbConnection {
 
     public static void main(String[] args){
-        //MongoClient = new MongoClients();
-
+        String connectionString = "mongodb+srv://hezekiahmunene:12345@cluster1.gfdfvl8.mongodb.net/?retryWrites=true&w=majority";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
