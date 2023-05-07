00d725ae751df7976edaa0b5daab6ebc62a8b724
Hezekiah Munene <hezemunene01@gmail.com>
2023-05-07 22:25:17 +0300

    cleared all statements to relearn how to connect to ATLAS

--- Lotto/src/main/java/LottoCompany/DbConnection.java
+++ Lotto/src/main/java/LottoCompany/DbConnection.java
@@ -1,6 +1,5 @@
 package LottoCompany;
 
-
 import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -10,45 +9,9 @@ import org.bson.Document;
 import org.bson.types.ObjectId;
 
 public class DbConnection {
-
     public static void main(String[] args){
 
-        String connectionString = "mongodb+srv://hezekiahmunene:12345@cluster1.gfdfvl8.mongodb.net/?retryWrites=true&w=majority";
-        ServerApi serverApi = ServerApi.builder()
-                .version(ServerApiVersion.V1)
-                .build();
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(connectionString))
-                .serverApi(serverApi)
-                .build();
-        // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            try {
-                // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase("admin");
-                database.runCommand(new Document("ping", 1));
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
 
-
-                /*try (MongoClient mongoClients = MongoClients.create(System.getProperty("mongodb.uri"))) {
-
-                    MongoDatabase sampleTrainingDB = mongoClients.getDatabase("LottoDB");
-                    MongoCollection<Document> gradesCollection = sampleTrainingDB.getCollection("LottoPlayers");
-
-
-                    Document student = new Document("_id", new ObjectId());
-                    student.append("username", "Sammy Musangi")
-                            .append("ID", "34456883");
-
-                    gradesCollection.insertOne(student);
-
-                }*/
-
-
-            } catch (MongoException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
 
--- Lotto/src/main/java/LottoCompany/main.java
+++ Lotto/src/main/java/LottoCompany/main.java
@@ -2,14 +2,14 @@ package LottoCompany;
 
 public class main {
     public static void main(String[] args){
-       /* LottoPlayer instance1 = new LottoPlayer();
+        LottoPlayer instance1 = new LottoPlayer();
         instance1.user_details();
-        instance1.make_selection();*/
+        instance1.make_selection();
 
         LottoSys instance2 = new LottoSys();
         instance2.create_gameid();
         instance2.generate_winning_numbers();
-        //instance2.compare_selection_winNumbs(instance1);
+        instance2.compare_selection_winNumbs(instance1);
         }
     }
 
